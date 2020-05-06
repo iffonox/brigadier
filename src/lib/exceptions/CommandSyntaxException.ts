6d6e18c35d736c798f92fdaf69948dd92b39e35a
Sebastian Rey <s.rey@pixelstein.de>
2020-05-06 18:56:42 +0200

    removed CommandSyntaxException base class, because of TypeScript design limitation (https://github.com/Microsoft/TypeScript/issues/13255)

--- src/lib/exceptions/CommandSyntaxException.ts
+++ src/lib/exceptions/CommandSyntaxException.ts
@@ -3,75 +3,74 @@ import BuiltInExceptions from "./BuiltInExceptions"
 import BuiltInExceptionProvider from "./BuiltInExceptionProvider"
 import CommandExceptionType from "./CommandExceptionType"
 
-export default class CommandSyntaxException extends Error {
-    
-    public static CONTEXT_AMOUNT = 10;    
+export default class CommandSyntaxException {
+
+    public static CONTEXT_AMOUNT = 10;
     public static BUILT_IN_EXCEPTIONS: BuiltInExceptionProvider = new BuiltInExceptions();
-    
-    private type: CommandExceptionType;    
-    private __message: Message;    
-    private input: string;    
-    private cursor: number;    
-    
+
+    private type: CommandExceptionType;
+    private __message: Message;
+    private input: string;
+    private cursor: number;
+    public message: string;
+
     public constructor (type: CommandExceptionType, message: Message, input: string = null, cursor = -1) {
-		super(message.getString());
-		Error.captureStackTrace(this, CommandSyntaxException)
         this.type = type;
         this.__message = message;
         this.input = input;
         this.cursor = cursor;
 
-        this.message = this.getMessage();
+        this.message = this.getMessage()
     }
-    
+
     public getMessage(): string {
         let message = this.__message.getString();
         let context = this.getContext();
         if (context != null) {
             message += " at position " + this.cursor + ": " + context;
-        }        
+        }
         return message;
     }
-    
+
     public getRawMessage(): Message {
         return this.__message;
     }
-    
+
     public getContext(): string {
         if (this.input == null || this.cursor < 0) {
             return null;
         }
-        
+
         let builder = ""
         let cursor = Math.min(this.input.length, this.cursor);
         if (cursor > CommandSyntaxException.CONTEXT_AMOUNT) {
             builder += "...";
         }
-        
+
         builder += this.input.substring(
-			Math.max(
-				0, 
-				cursor - CommandSyntaxException.CONTEXT_AMOUNT
-			),
-			cursor
-		);
+            Math.max(
+                0,
+                cursor - CommandSyntaxException.CONTEXT_AMOUNT
+            ),
+            cursor
+        );
         builder += "<--[HERE]";
         return builder;
     }
-    
+
     public getType(): CommandExceptionType {
         return this.type;
     }
-    
+
     public getInput(): string {
         return this.input;
     }
-    
+
     public getCursor(): number {
         return this.cursor;
     }
 
     public toString(): string {
-        return this.message;
+        return this.getMessage();
     }
 }
