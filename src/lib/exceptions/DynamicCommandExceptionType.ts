a59256397838f71765685ec9c059dc51c7b7ad33
Sebastian Rey <s.rey@pixelstein.de>
2020-05-26 17:46:34 +0200

    added condition to DynamicCommandExceptionType and SimpleCommandExceptionType before use of Error.captureStackTrace

--- src/lib/exceptions/DynamicCommandExceptionType.ts
+++ src/lib/exceptions/DynamicCommandExceptionType.ts
@@ -4,18 +4,21 @@ import CommandSyntaxException from "./CommandSyntaxException"
 import ImmutableStringReader from "../ImmutableStringReader"
 
 export default class DynamicCommandExceptionType implements CommandExceptionType {
-    
+
     private fn: Function;
-    
+
     public constructor(fn: (...args: any[]) => Message) {
 		this.fn = fn;
-		Error.captureStackTrace(this, DynamicCommandExceptionType)
+
+        if (Error.captureStackTrace) {
+            Error.captureStackTrace(this, DynamicCommandExceptionType)
+        }
     }
-    
+
     public create(...args: any[]): CommandSyntaxException {
         return new CommandSyntaxException(this, this.fn(...args));
     }
-    
+
     public createWithContext(reader: ImmutableStringReader, ...args: any[]): CommandSyntaxException {
         return new CommandSyntaxException(this, this.fn(...args), reader.getString(), reader.getCursor());
     }
--- src/lib/exceptions/SimpleCommandExceptionType.ts
+++ src/lib/exceptions/SimpleCommandExceptionType.ts
@@ -4,23 +4,26 @@ import CommandExceptionType from "./CommandExceptionType"
 import CommandSyntaxException from "./CommandSyntaxException";
 
 export default class SimpleCommandExceptionType implements CommandExceptionType {
-    
-    private message: Message;
-    
-    public constructor (message: Message) {
+
+	private message: Message;
+
+	public constructor(message: Message) {
 		this.message = message;
-		Error.captureStackTrace(this, SimpleCommandExceptionType)
-    }
-    
-    public create(): CommandSyntaxException {
-        return new CommandSyntaxException(this, this.message)
-    }
-    
-    public createWithContext(reader: ImmutableStringReader): CommandSyntaxException {
-        return new CommandSyntaxException(this, this.message, reader.getString(), reader.getCursor());
-    }
-    
-    public toString(): string {
-        return this.message.getString();
-    }
+
+		if (Error.captureStackTrace) {
+			Error.captureStackTrace(this, SimpleCommandExceptionType)
+		}
+	}
+
+	public create(): CommandSyntaxException {
+		return new CommandSyntaxException(this, this.message)
+	}
+
+	public createWithContext(reader: ImmutableStringReader): CommandSyntaxException {
+		return new CommandSyntaxException(this, this.message, reader.getString(), reader.getCursor());
+	}
+
+	public toString(): string {
+		return this.message.getString();
+	}
 }
